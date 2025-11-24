ef611c8e9b3024f3e1f9b0572696ae829001a8ab
danielhe4rt <danielhe4rt@gmail.com>
2025-11-24 11:56:34 -0300

    feat: add new modules and dependencies
    - Added He4rt Core and 3Pontos Portal modules.
    - Updated dependencies in `composer.json` and `composer.lock`.
    - Included new CSS resources in Vite configuration.
    - Added `mise.toml` with Node version specification.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/filament/admin/theme.css', 'resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/filament/admin/theme.css',
+                'app-modules/he4rt/resources/css/theme.css',
+                'app-modules/he4rt/resources/css/themes/3pontos/theme.css',
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
