e1f5e330cb4ed61a174b846a504a6524762d9532
kaster <diogokaster@gmail.com>
2025-11-24 14:58:34 -0300

    feat: added community section to homepage with detailed content and animations

--- app-modules/portal/resources/views/homepage.blade.php
+++ app-modules/portal/resources/views/homepage.blade.php
@@ -3,5 +3,6 @@
     <x-portal::sections.foundations />
     <x-portal::sections.social-action />
     <x-portal::sections.projects />
+    <x-portal::sections.community />
     <x-portal::sections.contact />
 </x-filament-panels::page>
