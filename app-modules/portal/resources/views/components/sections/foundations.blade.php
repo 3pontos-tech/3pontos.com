1290d1306732f775ffadc95258a963fe451b2680
kaster <diogokaster@gmail.com>
2025-11-24 14:02:52 -0300

    feat: added social action section and adjusted layouts in foundation and headline components

--- app-modules/portal/resources/views/components/sections/foundations.blade.php
+++ app-modules/portal/resources/views/components/sections/foundations.blade.php
@@ -15,61 +15,63 @@
         </div>
 
         <div class="flex flex-col gap-8">
-            <x-he4rt::headline>
-                <x-slot:title>Nossos pilares</x-slot>
-                <x-slot:description>
-                    A 3 Pontos conecta empresas e startups inovadoras a talentos excepcionais, acelerando soluções reais
-                    e transformando ideias em impacto.
-                </x-slot>
-                <x-slot:actions>
-                    <x-he4rt::button href="/">Button</x-he4rt::button>
-                </x-slot>
-            </x-he4rt::headline>
-        </div>
+            <div>
+                <x-he4rt::headline>
+                    <x-slot:title>Nossos pilares</x-slot>
+                    <x-slot:description>
+                        A 3 Pontos conecta empresas e startups inovadoras a talentos excepcionais, acelerando soluções
+                        reais e transformando ideias em impacto.
+                    </x-slot>
+                    <x-slot:actions>
+                        <x-he4rt::button href="/">Button</x-he4rt::button>
+                    </x-slot>
+                </x-he4rt::headline>
+            </div>
 
-        <div
-            x-data="{ visible: false }"
-            x-intersect.threshold.20="visible = true"
-            class="grid grid-cols-1 gap-8 lg:col-start-2"
-        >
-            <x-he4rt::card class="gap-4">
-                <x-slot:icon class="items-center gap-4">
-                    <x-he4rt::icon size="sm" class="rounded-sm" />
-                    <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
-                </x-slot>
-                <x-slot:description class="text-xs leading-[1.5]">
-                    Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos, desenvolvemos
-                    e implementamos soluções de ponta que transformam essas barreiras em oportunidades. Do
-                    desenvolvimento customizado ao marketing digital estratégico, nós somos o parceiro que sua empresa
-                    precisa para escalar com confiança.
-                </x-slot>
-            </x-he4rt::card>
+            <div
+                x-data="{ visible: false }"
+                x-intersect.threshold.20="visible = true"
+                class="grid grid-cols-1 gap-8"
+            >
+                <x-he4rt::card class="gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos,
+                        desenvolvemos e implementamos soluções de ponta que transformam essas barreiras em
+                        oportunidades. Do desenvolvimento customizado ao marketing digital estratégico, nós somos o
+                        parceiro que sua empresa precisa para escalar com confiança.
+                    </x-slot>
+                </x-he4rt::card>
 
-            <x-he4rt::card class="gap-4">
-                <x-slot:icon class="items-center gap-4">
-                    <x-he4rt::icon size="sm" class="rounded-sm" />
-                    <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
-                </x-slot>
-                <x-slot:description class="text-xs leading-[1.5]">
-                    Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos, desenvolvemos
-                    e implementamos soluções de ponta que transformam essas barreiras em oportunidades. Do
-                    desenvolvimento customizado ao marketing digital estratégico, nós somos o parceiro que sua empresa
-                    precisa para escalar com confiança.
-                </x-slot>
-            </x-he4rt::card>
+                <x-he4rt::card class="gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos,
+                        desenvolvemos e implementamos soluções de ponta que transformam essas barreiras em
+                        oportunidades. Do desenvolvimento customizado ao marketing digital estratégico, nós somos o
+                        parceiro que sua empresa precisa para escalar com confiança.
+                    </x-slot>
+                </x-he4rt::card>
 
-            <x-he4rt::card class="gap-4">
-                <x-slot:icon class="items-center gap-4">
-                    <x-he4rt::icon size="sm" class="rounded-sm" />
-                    <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
-                </x-slot>
-                <x-slot:description class="text-xs leading-[1.5]">
-                    Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos, desenvolvemos
-                    e implementamos soluções de ponta que transformam essas barreiras em oportunidades. Do
-                    desenvolvimento customizado ao marketing digital estratégico, nós somos o parceiro que sua empresa
-                    precisa para escalar com confiança.
-                </x-slot>
-            </x-he4rt::card>
+                <x-he4rt::card class="gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos,
+                        desenvolvemos e implementamos soluções de ponta que transformam essas barreiras em
+                        oportunidades. Do desenvolvimento customizado ao marketing digital estratégico, nós somos o
+                        parceiro que sua empresa precisa para escalar com confiança.
+                    </x-slot>
+                </x-he4rt::card>
+            </div>
         </div>
     </div>
 </section>
--- app-modules/portal/resources/views/homepage.blade.php
+++ app-modules/portal/resources/views/homepage.blade.php
@@ -1,5 +1,6 @@
 <x-filament-panels::page full-height="true">
     <x-portal::sections.hero />
     <x-portal::sections.foundations />
+    <x-portal::sections.social-action />
     <x-portal::sections.contact />
 </x-filament-panels::page>
