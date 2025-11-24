4101e0debd3f965696487d6847da2e8485d06cb8
kaster <diogokaster@gmail.com>
2025-11-24 14:46:50 -0300

    feat: revamped projects section layout, added new project descriptions and updated SVG graphics

--- app-modules/portal/resources/views/components/sections/projects.blade.php
+++ app-modules/portal/resources/views/components/sections/projects.blade.php
@@ -1,60 +1,83 @@
-<section class="hp-section" id="projects">
-    <div class="hp-container">
-        <x-he4rt::headline align="center" size="md" :keywords="['prática']">
-            <x-slot:badge>
-                <x-he4rt::badge>
-                    <x-filament::icon icon="heroicon-o-cube" class="h-5 w-5" />
-                    Nossos projetos
-                </x-he4rt::badge>
-            </x-slot>
+<section
+    class="hp-section bg-elevation-01dp/86 border-outline-dark relative max-h-[80vh] min-h-[80vh] overflow-hidden border-t border-b backdrop-blur-md"
+    id="about"
+>
+    <div
+        class="hp-container relative z-10 grid h-full grid-cols-1 gap-x-12 py-4 sm:py-6 md:py-8 lg:grid-cols-[1fr_2fr] lg:py-10 xl:py-16"
+    >
+        <div class="relative m-0 hidden h-full items-center justify-center lg:block">
+            <div class="absolute inset-0 h-full overflow-hidden">
+                <img src="{{ asset('images/3pontos/grids-vertical.svg') }}" alt="" class="h-full w-full" />
+            </div>
+        </div>
 
-            <x-slot:title>Aprenda na prática</x-slot>
+        <div class="flex h-full min-h-0 flex-col items-start gap-4">
+            <div>
+                <x-he4rt::headline size="sm">
+                    <x-slot:badge>
+                        <x-he4rt::section-title size="sm">Projetos</x-he4rt::section-title>
+                    </x-slot>
+                    <x-slot:title>Aonde a Disrupção acontece</x-slot>
+                    <x-slot:description>
+                        Conheça alguns dos projetos que nasceram ou foram acelerados dentro do nosso ecossistema. Aqui,
+                        a tecnologia se traduz em resultados reais e impacto no mercado, mostrando a sinergia entre
+                        nossa aceleradora e a comunidade de talentos.
+                    </x-slot>
+                </x-he4rt::headline>
+            </div>
 
-            <x-slot:description>
-                Contribua com projetos open source e desenvolva habilidades reais enquanto constrói seu portfólio.
-            </x-slot>
-        </x-he4rt::headline>
+            <div
+                class="flex h-full flex-1 flex-col items-start gap-8 overflow-y-auto mask-t-from-95% mask-t-to-99% mask-b-from-95% mask-b-to-99% p-1 py-4 lg:pr-8"
+            >
+                <x-he4rt::card class="h-fit gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Ipê</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Uma plataforma de investimentos intuitiva e acessível, focada em democratizar o acesso ao
+                        mercado financeiro. Com uma interface amigável e integração segura com APIs. Ela torna o
+                        processo de investimento simples e seguro para todos.
+                    </x-slot>
+                </x-he4rt::card>
 
-        <div class="grid w-full grid-cols-1 items-center justify-center gap-8 sm:grid-cols-3 lg:gap-16">
-            <x-he4rt::card>
-                <x-slot:title>4Noobs</x-slot>
-                <x-slot:description>Repositórios de conteúdos sobre diversas tecnologias.</x-slot>
-                <x-slot:tags>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                </x-slot>
-                <x-slot:actions>
-                    <x-he4rt::button block>Ver projeto</x-he4rt::button>
-                </x-slot>
-            </x-he4rt::card>
+                <x-he4rt::card class="h-fit gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Flamma</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Clube de benefícios exclusivo que vai além, oferecendo consultoria financeira personalizada e
+                        acesso a produtos e serviços com condições especiais. É a solução ideal para quem busca otimizar
+                        suas finanças e ter acesso a vantagens únicas no mercado.
+                    </x-slot>
+                </x-he4rt::card>
 
-            <x-he4rt::card>
-                <x-slot:title>He4rtLabs</x-slot>
-                <x-slot:description>
-                    Projetos práticos desenvolvidos pela comunidade para aprendizado.
-                </x-slot>
-                <x-slot:tags>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                </x-slot>
-                <x-slot:actions>
-                    <x-he4rt::button block>Ver projeto</x-he4rt::button>
-                </x-slot>
-            </x-he4rt::card>
+                <x-he4rt::card class="h-fit gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Flare</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        serviço digital e ágil que oferece avaliação e diagnóstico financeiro detalhado. Utilizando um
+                        algoritmo avançado de análise de dados, ele permite que os clientes recebam um parecer
+                        financeiro completo em curto período de tempo.
+                    </x-slot>
+                </x-he4rt::card>
 
-            <x-he4rt::card>
-                <x-slot:title>Desafios de Código</x-slot>
-                <x-slot:description>
-                    Desafios semanais para praticar suas habilidades de programação.
-                </x-slot>
-                <x-slot:tags>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                    <x-he4rt::tag>Documentação</x-he4rt::tag>
-                </x-slot>
-                <x-slot:actions>
-                    <x-he4rt::button block>Ver projeto</x-he4rt::button>
-                </x-slot>
-            </x-he4rt::card>
+                <x-he4rt::card class="h-fit gap-4">
+                    <x-slot:icon class="items-center gap-4">
+                        <x-he4rt::icon size="sm" class="rounded-sm" />
+                        <x-he4rt::heading size="2xs">Soluções Estratégicas para Seu Crescimento</x-he4rt::heading>
+                    </x-slot>
+                    <x-slot:description class="text-xs leading-[1.5]">
+                        Sua empresa tem uma visão clara, mas enfrenta desafios tecnológicos. Aqui na 3 Pontos,
+                        desenvolvemos e implementamos soluções de ponta que transformam essas barreiras em
+                        oportunidades. Do desenvolvimento customizado ao marketing digital estratégico, nós somos o
+                        parceiro que sua empresa precisa para escalar com confiança.
+                    </x-slot>
+                </x-he4rt::card>
+            </div>
         </div>
     </div>
 </section>
--- app-modules/portal/resources/views/homepage.blade.php
+++ app-modules/portal/resources/views/homepage.blade.php
@@ -2,5 +2,6 @@
     <x-portal::sections.hero />
     <x-portal::sections.foundations />
     <x-portal::sections.social-action />
+    <x-portal::sections.projects />
     <x-portal::sections.contact />
 </x-filament-panels::page>
