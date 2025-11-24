f8a4d2e0aa77253f204f96404771cf87178ad9f6
kaster <diogokaster@gmail.com>
2025-11-24 15:10:00 -0300

    feat: added events section to homepage with descriptive content and new design elements

--- app-modules/portal/resources/views/homepage.blade.php
+++ app-modules/portal/resources/views/homepage.blade.php
@@ -4,5 +4,6 @@
     <x-portal::sections.social-action />
     <x-portal::sections.projects />
     <x-portal::sections.community />
+    <x-portal::sections.events />
     <x-portal::sections.contact />
 </x-filament-panels::page>
