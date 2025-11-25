feb2335efd1a2b483cb241aa6170f6d77723db94
danielhe4rt <danielhe4rt@gmail.com>
2025-11-25 12:28:20 -0300

    feat: metatags

---
+++ resources/views/components/metatags.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'description',
+    'url',
+    'title',
+    'coverImage',
+])
+
+<meta name="title" content="{{ $title }}" />
+<meta name="description" content="{{ $description }}" />
+
+<meta property="og:description" content="{{ $description }}" />
+<meta property="og:url" content="{{ $url }}" />
+<meta property="og:title" content="{{ $title }}" />
+<meta property="og:image" content="{{ $coverImage }}" />
+<meta property="og:type" content="website" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="{{ $title }}" />
+<meta name="twitter:description" content="{{ $description }}" />
+<meta name="twitter:image" content="{{ $coverImage }}" />
